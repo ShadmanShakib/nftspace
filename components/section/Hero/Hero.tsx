f9ca6b854d627a3877072b4dad69990eb055fd17
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-07 10:28:54 +0600

    classnames

--- components/section/Hero/Hero.tsx
+++ components/section/Hero/Hero.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import { LinkButton } from "@components/ui";
 function Hero() {
   return (
     <div className={styles.main}>
       <h1>Discover, collect, and sell extraodinary NFTs</h1>
       <span className="">On the worlds first and largest NFT marketplace</span>
-      <div></div>
+      <div>
+        <LinkButton varient="primary">Explore</LinkButton>
+        <LinkButton varient="secondary">Create</LinkButton>
+      </div>
     </div>
   );
 }
