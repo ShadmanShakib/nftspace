9102bc74929642b66619afe235a395ec729691c4
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-08 09:18:43 +0600

    InfoIcon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as MenuIcon } from "./MenuIcon";
 export { default as SearchIcon } from "./Search";
 export { default as CloseIcon } from "./CloseIcon";
 export { default as WalletIcon } from "./WalletIcon";
+export { default as InfoIcon } from "./InfoIcon";
