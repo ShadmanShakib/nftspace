8a35c5acf2eccde8d721361e216157c7b82667e0
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-08 09:53:26 +0600

    Featured card

---
+++ components/section/Featured/Card.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import s from "./Featured.module.css";
+function Card() {
+  return (
+    <div className={s.card}>
+      <img
+        src="https://storage.opensea.io/static/promocards/rhymeslikedimez-promo.jpeg"
+        alt="featured"
+      />
+      <div className={s.cardfooter}>
+        <h3>Rhyme Slikedimez</h3>
+        <p>Meet me on cloud nine, an art series</p>
+        <button>live</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
