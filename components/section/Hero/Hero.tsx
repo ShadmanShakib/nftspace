17c4da67214b42a38a5f787c23e4a152c31012d9
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-08 09:18:58 +0600

    Hero

--- components/section/Hero/Hero.tsx
+++ components/section/Hero/Hero.tsx
@@ -1,20 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from "./Hero.module.css";
 import { LinkButton } from "@components/ui";
 function Hero() {
   return (
     <div className={styles.main}>
-      <h1 className={styles.title}>
-        Discover, collect, and sell extraodinary NFTs
-      </h1>
-      <h2 className={styles.subtitle}>
-        On the worlds first and largest NFT
-        <br /> marketplace
-      </h2>
-      <div className={styles.buttons}>
-        <LinkButton varient="primary">Explore</LinkButton>
-        <div className="w-6" />
-        <LinkButton varient="secondary">Create</LinkButton>
+      <div className={styles.titleSection}>
+        <h1 className={styles.title}>
+          Discover, collect, and sell extraodinary NFTs
+        </h1>
+        <h2 className={styles.subtitle}>
+          On the worlds first and largest NFT
+          <br /> marketplace
+        </h2>
+        <div className={styles.buttons}>
+          <LinkButton varient="primary">Explore</LinkButton>
+          <div className="w-6" />
+          <LinkButton varient="secondary">Create</LinkButton>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img
+          className={styles.image}
+          alt="Feature Image"
+          src="https://lh3.googleusercontent.com/LPL9OgXmxE9ZrxeN-cGj01hOQGFM6MoHGE1qEPapFKzVIaaF-erA9JLdoOXBVR2GIYjB4J6xX1BFL27kkyQewkTZtsP3CdPiqX-qow=s0"
+        />
+        <div className={styles.cardfooter}>
+          <img
+            src="https://lh3.googleusercontent.com/BdxvLseXcfl57BiuQcQYdJ64v-aI8din7WPk0Pgo3qQFhAUH-B6i-dCqqc_mCkRIzULmwzwecnohLhrcH8A9mpWIZqA7ygc52Sr81hE=s130"
+            alt="author"
+          />
+          <div className={styles.author}>
+            <h1>CryptoPunk #4648</h1>
+            <h2>CryptoPunks</h2>
+          </div>
+          <button aria-label="Get featured" type="button"></button>
+        </div>
       </div>
     </div>
   );
