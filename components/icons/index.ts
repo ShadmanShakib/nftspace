6cb662fff833f1b4077a91f33527cf77bdb04cb7
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-23 08:00:11 +0600

    Social icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -4,3 +4,8 @@ export { default as CloseIcon } from "./CloseIcon";
 export { default as WalletIcon } from "./WalletIcon";
 export { default as InfoIcon } from "./InfoIcon";
 export { default as Ethereum } from "./Ethereum";
+export { default as Twitter } from "./Twitter";
+export { default as Instagram } from "./Instagram";
+export { default as Reddit } from "./Reddit";
+export { default as Discord } from "./Discord";
+export { default as YouTube } from "./YouTube";
