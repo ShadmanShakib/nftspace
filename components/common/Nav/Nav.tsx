e2f18d4a09b26b9323aab7026ae12a6025f662fc
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-29 18:18:18 +0600

    backdrop filter

--- components/common/Nav/Nav.tsx
+++ components/common/Nav/Nav.tsx
@@ -5,35 +5,32 @@ import { useUI } from "libs/hooks";
 export default function Nav() {
   const { isOpen, toggleMenu } = useUI();
   return (
-    <div className={styles.main}>
-      <div className={styles.logo}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="	https://opensea.io/static/images/logos/opensea.svg"
-          alt="logo"
-        />
-        <h1>Nftspace</h1>
-      </div>
-      <div className={styles.searchbox}>
-        <SearchIcon className="menuicon" />
-        <input
-          type="text"
-          placeholder="Search items, collection, and accounts"
-        />
-      </div>
-      <div className={styles.menu}>
-        <h1 className={styles.menuItem}>Explore</h1>
-        <h1 className={styles.menuItem}>Stats</h1>
-        <h1 className={styles.menuItem}>Accounts</h1>
-        <h1 className={styles.menuItem}>Create</h1>
-        <WalletIcon className="h-6  w-6 m-5" />
-      </div>
-      <div onClick={toggleMenu} className={styles.menuicon}>
-        {isOpen ? (
-          <CloseIcon className="fill-current" />
-        ) : (
-          <MenuIcon className="fill-current" />
-        )}
+    <div className="relative">
+      <div className={styles.main}>
+        <div className={styles.logo}>
+          <h1 className="font-bold text-lg text-white">NFTSpace</h1>
+        </div>
+        <div className={styles.searchbox}>
+          <SearchIcon className="menuicon" />
+          <input
+            type="text"
+            placeholder="Search items, collection, and accounts"
+          />
+        </div>
+        <div className={styles.menu}>
+          <h1 className={styles.menuItem}>Explore</h1>
+          <h1 className={styles.menuItem}>Stats</h1>
+          <h1 className={styles.menuItem}>Accounts</h1>
+          <h1 className={styles.menuItem}>Create</h1>
+          <WalletIcon className="h-6  w-6 m-5" />
+        </div>
+        <div onClick={toggleMenu} className={styles.menuicon}>
+          {isOpen ? (
+            <CloseIcon className="fill-current" />
+          ) : (
+            <MenuIcon className="fill-current" />
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ pages/collections/CryptoPunks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CryptoPunks() {
+  return <div></div>;
+}
+
+export default CryptoPunks;
