f95d3a18697224005a8d29766957026c7646b90f
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-30 12:15:56 +0600

    Collection Header C

---
+++ components/collection/CollectionBody/CollectionBody.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AiOutlineAppstore } from "react-icons/ai";
+function CollectionBody() {
+  return (
+    <div>
+      <div className="">
+        <div className="">
+          <AiOutlineAppstore size={20} />
+          <h1>Items</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CollectionBody;
--- components/collection/CollectionHeader/CollectionHeader.tsx
+++ components/collection/CollectionHeader/CollectionHeader.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 import { FiMoreVertical } from "react-icons/fi";
 import DropDown from "./DropDown";
 import { VarifiedIcon } from "@components/icons";
-
+import { Ethereum } from "@components/icons";
 function CollectionHeader() {
   const [showDropDown, setShowDropDown] = React.useState(false);
   return (
-    <div>
+    <div className={s.main}>
       {/*eslint-disable-next-line @next/next/no-img-element */}
       <img
         alt="Banner Image"
@@ -42,23 +42,35 @@ function CollectionHeader() {
           <VarifiedIcon className="h-8 w-8 fill-current text-blue-700" />
         </div>
         {/* Stats Box */}
-        <div className="">
-          <div>
+        <div className={s.statsbox}>
+          <div className={s.box}>
             <h1> 10.0K </h1>
             <p>items</p>
           </div>
-          <div className="">
+          <div className={s.box}>
             <h1>3.3K</h1>
             <p>owners</p>
           </div>
-          <div className="">
+          <div className={s.box}>
             <h1>---</h1>
             <p>floor price</p>
           </div>
-          <div className="">
-            <h1>761.2k</h1>
+          <div className={s.box}>
+            <div className={s.iconbox}>
+              <Ethereum className="h-6 w-4" />
+              <h1 className="text-center">761.2k</h1>
+            </div>
+
             <p>volume traded</p>
           </div>
+        </div>
+        <div className=" text-center mt-6 px-6">
+          <span>
+            CryptoPunks launched as a fixed set of 10,000 items in mid-2017 and
+            became one of the inspirations for the ERC-721 standard. They have
+            been featured in places like The New York Times, Christie’s of
+            London, Art|Basel Miami, and The PBS NewsHour.
+          </span>
         </div>
       </div>
     </div>
