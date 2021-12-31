5d438eb68881e8841f14c9cf4d480eb8941b021b
Shadman Shakib <sm.shakib007@gmail.com>
2021-12-31 17:10:55 +0600

    Category Card

--- components/section/TopCategories/TopCategories.tsx
+++ components/section/TopCategories/TopCategories.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { CategoryCard } from "@components/ui/Cards";
+import s from "./TopCategories.module.css";
 function TopCategories() {
   return (
-    <div>
-      <CategoryCard />
+    <div className={s.root}>
+      <CategoryCard name="Collections" />
     </div>
   );
 }
--- components/ui/Cards/CategoryCard/CategoryCard.tsx
+++ components/ui/Cards/CategoryCard/CategoryCard.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import s from "./CategoryCard.module.css";
-function CategoryCard() {
+interface Props {
+  name: string;
+}
+function CategoryCard(props: Props) {
+  const { name } = props;
   return (
     <div className={s.main}>
       <Image
+        className={s.img}
         src="/images/domain-name.png"
-        width={200}
+        width={300}
         height={200}
         alt="Domain name"
       />
+      <div className={s.bottom}>
+        <h1 className={s.name}>{name}</h1>
+      </div>
     </div>
   );
 }
