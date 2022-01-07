6959c54f6b1b534d307058edd5b1589dc7b89e5b
Shadman Shakib <sm.shakib007@gmail.com>
2022-01-07 20:25:30 +0600

    responsive nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         "primary-dark": "#0a092d",
         "primary-light": "#F2F2F2",
         "primary-dark-hover": "#0D0D0D",
+        bglight: "#353840",
         "btn-bg": "#262b2f",
         border1: "#151b22",
       },
