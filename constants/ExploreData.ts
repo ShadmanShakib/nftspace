0309057598f4c8c03673fefc3981a5d33b444121
Shadman Shakib <sm.shakib007@gmail.com>
2022-01-09 09:04:56 +0600

    Explore data

--- constants/ExploreData.ts
+++ constants/ExploreData.ts
@@ -9,3 +9,12 @@ export const EXPLORE_DATA = [
   "Sports",
   "Treading Cards",
 ];
+
+export const ExploreList = [
+  {
+    id: 1,
+    title: "Trending",
+    img: "https://lh3.googleusercontent.com/OwmR2aAFXTNxnPAiKrOhbsfZSSQqoaGMFQvedFileV6Vv-9TPs7TFI8RTXdIkoqfc9AZhFI4XcTHREnPc3mc-MDKFC4qapJbOyhcQQ=h200",
+    logo: "https://lh3.googleusercontent.com/R3b_Ju-BF7Ae45pp1f7UxCS5wF06dfFG7ydux_v9S8lJ7CL3j4kgv7a0nM4yVw-GhOH21ZigeaNluK-nuo6Dclq9LdQYH2Cvj8PfMQ=s100",
+  },
+];
