e3646c1ff3cf57ede5189aab2e169cc17deebc95
Shadman Shakib <sm.shakib007@gmail.com>
2022-01-09 11:25:48 +0600

    icons added

--- components/section/Info/Card.tsx
+++ components/section/Info/Card.tsx
@@ -4,13 +4,13 @@ import s from "./Info.module.css";
 interface Props {
   title: string;
   description: string;
-  url?: string;
+  url: string;
 }
 export default function Card(props: Props) {
   const { title, description, url } = props;
   return (
     <div className={s.card}>
-      <img src={url} alt="" className="" />
+      <img src={url} alt="Logo" className="h-10 w-10" />
       <div className={s.card_body}>
         <h5 className={s.card_title}>{title}</h5>
 
