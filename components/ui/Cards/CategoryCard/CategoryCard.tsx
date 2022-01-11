f261dab62f6663b95d0207a1d5b1ed95c2d83dad
Shadman Shakib <sm.shakib007@gmail.com>
2022-01-11 11:16:54 +0600

    Collection link

--- components/ui/Cards/CategoryCard/CategoryCard.tsx
+++ components/ui/Cards/CategoryCard/CategoryCard.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import Image from "next/image";
 import s from "./CategoryCard.module.css";
+import Link from "next/link";
 interface Props {
   name: string;
 }
 function CategoryCard(props: Props) {
   const { name } = props;
   return (
-    <div className={s.main}>
-      <Image
-        className={s.img}
-        src="/images/domain-name.png"
-        width={350}
-        height={200}
-        alt="Domain name"
-      />
-      <div className={s.bottom}>
-        <h1 className={s.name}>{name}</h1>
+    <Link href="/collections/id" passHref>
+      <div className={s.main}>
+        <Image
+          className={s.img}
+          src="/images/domain-name.png"
+          width={350}
+          height={200}
+          alt="Domain name"
+        />
+        <div className={s.bottom}>
+          <h1 className={s.name}>{name}</h1>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
